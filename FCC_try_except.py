c082d112f10b6e58be36d249e3c37f5976db5d5a
ednayssell7 <ednayrr@gmail.com>
2021-01-15 20:46:43 -0700

    try, except, quit example

--- FCC_try_except.py
+++ FCC_try_except.py
@@ -1,17 +1,24 @@
 hours_worked = input(f'how many hours worked today?: ')
 hour_rate = input(f'what is the rate?:')
-hours_worked = float(hours_worked)
-hour_rate = float(hour_rate)
+try:
+    hours_worked = float(hours_worked)
+    hour_rate = float(hour_rate)
+except:
+    print(f'error, please enter a numeric input')
+    quit()
 
 def payment(hours, rate):
     if hours > 8:
         pay = hours * rate
+        pay = float(pay)
         extra_pay = (hours - 8) * (rate*0.5)
         salary = pay + extra_pay
+        salary = float(salary)
         print(f'Base Salary: ${pay} + extra hour payment: ${extra_pay}')
         return salary
     else:
         salary = hours * rate
+        salary = float(salary)
         return salary 
 
 final_pay= payment(hours_worked, hour_rate)
