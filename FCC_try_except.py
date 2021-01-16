c647bda8f95c4cae6700a6b6ab75be56ab164a47
ednayssell7 <ednayrr@gmail.com>
2021-01-15 19:29:06 -0700

    using functions to calculate salary and extra pays

---
+++ FCC_try_except.py
@@ -0,0 +1,20 @@
+hours_worked = input(f'how many hours worked today?: ')
+hour_rate = input(f'what is the rate?:')
+hours_worked = float(hours_worked)
+hour_rate = float(hour_rate)
+
+def payment(hours, rate):
+    if hours > 8:
+        pay = hours * rate
+        extra_pay = (hours - 8) * (rate*0.5)
+        salary = pay + extra_pay
+        print(f'Base Salary: ${pay} + extra hour payment: ${extra_pay}')
+        return salary
+    else:
+        salary = hours * rate
+        return salary 
+
+
+final_pay= payment(hours_worked, hour_rate)
+
+print(f'Total salary for the day: $', final_pay)
